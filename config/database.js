502894d0ddafadd75aa4ae62d0b829de0743c2b2
Anthony Salmeron <55327777+AnthonySalmeron@users.noreply.github.com>
2019-12-10 18:11:13 -0500

    Delete database.js

--- config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-
-    'url' : 'mongodb+srv://hireMe:vFsaAq4xwBTDNa3@cluster0-5pyfn.azure.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'test'
-};
